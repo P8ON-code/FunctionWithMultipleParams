c07fc22f821566de3b851f21815fffee17e44de4
P8ON-code <papasteryd8@gmail.com>
2022-12-04 15:52:07 +0100

    Dodaj pliki projektów.

---
+++ FunctionWithMultipleParams/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FunctionWithMultipleParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringsPrinter("T1", "T2", "T3");
+            NumberAdder(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        }
+
+        static void StringsPrinter(params string[] strings) //Function which takes not specified number of strings
+        {
+            foreach (var item in strings)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void NumberAdder(params int[] numbers)
+        {
+            int solution = 0;
+            foreach (var item in numbers)
+            {
+                solution+= item;
+            }
+            Console.WriteLine("All numbers added, give: {0}", solution);
+        }
+
+        // func(params type[] name)
+    }
+}
